af0d91ccd9301768652a5f969183b45190ce3bc9
Unko <kobie.watkins@gmail.com>
2020-09-16 01:36:51 -0400

    added the first router.post code, it should work

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,14 @@
 const router = require("express").Router();
 const Workout = require("../model/Workourt.js");
+const { db } = require("../models/workout.js");
+
+// api routes(always a difficult task)
+router.post("/api/workouts", async (req, res) => {
+  db.Workout.find({})
+    .then((workout) => {
+      res.json(workout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
